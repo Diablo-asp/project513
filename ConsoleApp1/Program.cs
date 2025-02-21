e8951951a2617f3e718ea1b18f64d03e6a9d9d84
Mohamed Said <Mohamed Said@DESKTOP-HQC9UF0>
2025-02-21 14:59:41 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // variabls
+            int value1 = 99;
+            int value2 = 99;
+            int value3 = 99;
+            int value4 = 99;
+            int value5 = 99;
+                    
+        }
+    }
+}
